af6524bd87cbef62b6683c3f96a39641f772670b
Andrea <andreaguzzon26@gmail.com>
2019-05-19 15:15:11 +0200

    Update player.rb

--- player.rb
+++ player.rb
@@ -2,10 +2,10 @@ require 'gosu'
 
 class Player
 		
-	HEIGHT = 80
-	WIDTH = 10
+  HEIGHT = 80
+  WIDTH = 10
 
-	attr_reader :x
+  attr_reader :x
   attr_reader :y
   attr_reader :score
   attr_reader :name
@@ -14,25 +14,25 @@ class Player
     @window = window
     @init_x = x
     @init_y = y
-		@x = x
+    @x = x
     @y = y
     @name = name
-		@score = 0
+    @score = 0
   end
 
-	def move_up
-		@y -= 5
-		@y = [HEIGHT/2, @y].max
-	end
+  def move_up
+    @y -= 5
+    @y = [HEIGHT/2, @y].max
+  end
 
-	def move_down
-		@y += 5
-		@y = [400 - HEIGHT/2, @y].min
-	end
+  def move_down
+    @y += 5
+    @y = [400 - HEIGHT/2, @y].min
+  end
 
-	def hit_ball(ball)
-		(@x - ball.x).abs < 5 && (@y - ball.y).abs < 50
-	end
+  def hit_ball(ball)
+    (@x - ball.x).abs < 5 && (@y - ball.y).abs < 50
+  end
 
   def reset
     @x = @init_x
@@ -43,13 +43,13 @@ class Player
     @score += 1
   end
 
-	def draw
-		@window.draw_quad(
-			@x - WIDTH/2, @y - HEIGHT/2, Color::WHITE,
-     	@x + WIDTH/2, @y - HEIGHT/2, Color::WHITE,
+  def draw
+    @window.draw_quad(
+      @x - WIDTH/2, @y - HEIGHT/2, Color::WHITE,
+      @x + WIDTH/2, @y - HEIGHT/2, Color::WHITE,
       @x + WIDTH/2, @y + HEIGHT/2, Color::WHITE,
-			@x - WIDTH/2, @y + HEIGHT/2, Color::WHITE
-		)
-	end
+      @x - WIDTH/2, @y + HEIGHT/2, Color::WHITE
+    )
+  end
  
 end
